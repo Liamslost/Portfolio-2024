06855bc614714773d2949949ec70b845f1e52225
Liam O'Connell <liamslost@googlemail.com>
2024-11-16 16:30:42 +0000

    styling and colours added. added links to git and LI in nav. added custom clover favicon

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict"
+
+window.onscroll = function() {navScrollFunction()};
+
+function navScrollFunction() {
+    if (document.body.scrollTop >50 || document.documentElement.scrollTop > 50) {
+        let nav = document.getElementById("nav")
+        nav.style.backgroundColor = "var(--color-navbar-bg)"
+    }else {
+        let nav = document.getElementById("nav")
+        nav.style.backgroundColor = ""
+    }
+}
