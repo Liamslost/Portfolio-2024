3d131b404dde1065ee28eb1104341f17f6016eb1
Liam O'Connell <liamslost@googlemail.com>
2024-11-17 14:26:56 +0000

    add loop for project hydration

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-"use strict"
+"use strict";
 
-window.onscroll = function() {navScrollFunction()};
+window.onscroll = function () {
+  navScrollFunction();
+};
 
 function navScrollFunction() {
-    if (document.body.scrollTop >50 || document.documentElement.scrollTop > 50) {
-        let nav = document.getElementById("nav")
-        nav.style.backgroundColor = "var(--color-navbar-bg)"
-    }else {
-        let nav = document.getElementById("nav")
-        nav.style.backgroundColor = ""
-    }
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    let nav = document.getElementById("nav");
+    nav.style.backgroundColor = "var(--color-navbar-bg)";
+  } else {
+    let nav = document.getElementById("nav");
+    nav.style.backgroundColor = "";
+  }
+}
+
+const Projects = [
+  { id: "1", name: "Project 1", description: "this is project 1", image: " " },
+  { id: "2", name: "Project 2", description: "this is project 2", image: " " },
+  { id: "3", name: "Project 3", description: "this is project 3", image: " " },
+  { id: "4", name: "Project 4", description: "this is project 4", image: " " },
+];
+
+function displayProjects(){
+
+    let output = "";
+    Projects.forEach(project => {
+
+        output += `<div class='projectTile tile ${project['id']}'>`;
+        output += `<h1>${project['description']}</h1>`;
+        output += `<p></p>`;
+        output += `<img src='${project['image']}'>`;
+        output += `</div>`;
+        
+    });
 }
