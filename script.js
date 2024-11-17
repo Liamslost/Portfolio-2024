57336c5b21b364c45ef08cc48d5ab86a57bdba9f
Liam O'Connell <liamslost@googlemail.com>
2024-11-17 17:00:11 +0000

    add JS render of projects array still needs styling and actual content adding

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 "use strict";
 
+const Projects = [
+  { id: "1", name: "Project 1", description: "this is project 1", image: " " },
+  { id: "2", name: "Project 2", description: "this is project 2", image: " " },
+  { id: "3", name: "Project 3", description: "this is project 3", image: " " },
+  { id: "4", name: "Project 4", description: "this is project 4", image: " " },
+];
+
 window.onscroll = function () {
   navScrollFunction();
 };
@@ -14,23 +21,24 @@ function navScrollFunction() {
   }
 }
 
-const Projects = [
-  { id: "1", name: "Project 1", description: "this is project 1", image: " " },
-  { id: "2", name: "Project 2", description: "this is project 2", image: " " },
-  { id: "3", name: "Project 3", description: "this is project 3", image: " " },
-  { id: "4", name: "Project 4", description: "this is project 4", image: " " },
-];
+window.addEventListener("DOMContentLoaded", () => {
+  displayProjects();
+});
 
-function displayProjects(){
+function displayProjects() {
+  let output = "";
+  let projectsContainer = document.querySelector(".projectsContainer");
+  Projects.forEach((project) => {
+    output += `<div class='tile tile${project["id"]}'>`;
+    output += `<h1>${project["description"]}</h1>`;
+    output += `<p></p>`;
+    output += `<img src='${project["image"]}'>`;
+    output += `</div>`;
+  });
 
-    let output = "";
-    Projects.forEach(project => {
-
-        output += `<div class='projectTile tile ${project['id']}'>`;
-        output += `<h1>${project['description']}</h1>`;
-        output += `<p></p>`;
-        output += `<img src='${project['image']}'>`;
-        output += `</div>`;
-        
-    });
+  if (!projectsContainer) {
+    console.error("projectsContainer element not found in the DOM");
+  } else {
+    projectsContainer.innerHTML = output;
+  }
 }
