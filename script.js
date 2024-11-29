c9f5f194134379d9b1f5726bc1d3fd08594ce3d5
Liam O'Connell <liamslost@googlemail.com>
2024-11-29 12:51:31 +0000

    stylinmg adjustments - need a lot more.  added some projects UX. added PNG for projects tiles. to top function for home button.

--- script.js
+++ script.js
@@ -3,28 +3,49 @@
 const Projects = [
   {
     id: "1",
-    name: "Splitter",
-    description: "this is project 1",
-    image: "Images/splitter.png",
+    h1: "Splitter",
+    h2: "this is project 1",
+    description:
+      "Life finds a way. What do they got in there? King Kong? You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager? Eventually, you do plan to have dinosaurs on your dinosaur tour, right? Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should.",
+    technologies: "",
+    image: "Images/split.png",
+    projectUrl: "",
+    githubUrl: "https://github.com/Liamslost/splitter",
   },
   {
     id: "2",
-    name: "To-do",
-    description: "this is project 2",
-    image: "Images/screenshot.png",
+    h1: "Price Calculator",
+    h2: "this is project 2",
+    description:
+      "Life finds a way. What do they got in there? King Kong? You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager? Eventually, you do plan to have dinosaurs on your dinosaur tour, right? Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should.",
+    technologies: "",
+    image: "Images/traffic-calculator.png",
+    projectUrl: "",
+    githubUrl: "https://github.com/Liamslost/priceCalculator",
   },
   {
     id: "3",
-    name: "Project 3",
-    description: "this is project 3",
-    image: "Images/screenshot.png",
+    h1: "Slim To-do App",
+    h2: "this is project 3",
+    description:
+      "Life finds a way. What do they got in there? King Kong? You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager? Eventually, you do plan to have dinosaurs on your dinosaur tour, right? Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should.",
+
+    technologies: "",
+    image: "Images/to-do.png",
+    projectUrl: "",
+    githubUrl: "https://github.com/Liamslost/slim-to-do-app",
   },
   {
     id: "4",
-    name: "Project 4",
-    description: "this is project 4",
-    image: "Images/screenshot.png",
-  }
+    h1: "Collections",
+    h2: "this is project 4",
+    description:
+      "Life finds a way. What do they got in there? King Kong? You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager? Eventually, you do plan to have dinosaurs on your dinosaur tour, right? Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should.",
+    technologies: "",
+    image: "Images/Placeholder.png",
+    projectUrl: "",
+    githubUrl: "https://github.com/Liamslost/CollectionsApp",
+  },
 ];
 
 const Progress = [
@@ -56,6 +77,7 @@ const Progress = [
 
 const CountProjects = Projects.length;
 const CountProgress = Progress.length;
+const ScrollContainer = document.querySelector(".progressContainer");
 
 window.onscroll = function () {
   navScrollFunction();
@@ -69,8 +91,8 @@ function navScrollFunction() {
     let nav = document.getElementById("nav");
     nav.style.backgroundColor = "";
   }
-  
 }
+
 window.addEventListener("DOMContentLoaded", () => {
   projectsCounter();
   progressCounter();
@@ -78,29 +100,34 @@ window.addEventListener("DOMContentLoaded", () => {
   displayProgress();
 });
 
-function projectsCounter() {
-let projectsCounter = document.querySelector('#projectsCounter');
-const targetProjectsCount = CountProjects;
-let currentProjectsCount = 20;
-
-if (!progressCounter) {
-  console.error(" #projectsCounter element not found in DOM");
-  return;
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
-const interval = setInterval(()=>{
-  currentProjectsCount--;
-  projectsCounter.textContent = `${currentProjectsCount}`;
-  if (currentProjectsCount <= targetProjectsCount) {
-    clearInterval(interval);
+
+function projectsCounter() {
+  let projectsCounter = document.querySelector("#projectsCounter");
+  const targetProjectsCount = CountProjects;
+  let currentProjectsCount = 20;
+
+  if (!progressCounter) {
+    console.error(" #projectsCounter element not found in DOM");
+    return;
   }
-}, 100);
+  const interval = setInterval(() => {
+    currentProjectsCount--;
+    projectsCounter.textContent = `${currentProjectsCount}`;
+    if (currentProjectsCount <= targetProjectsCount) {
+      clearInterval(interval);
+    }
+  }, 100);
 }
 
 function progressCounter() {
   let inProgress = document.querySelector("#progressCounter");
   const targetProgressCount = CountProgress;
   let currentProgressCount = 20;
-  
+
   if (!inProgress) {
     console.error("#progressCounter element not found in the DOM");
     return;
@@ -119,10 +146,17 @@ function displayProjects() {
   let output = "";
   let projectsContainer = document.querySelector(".projectsContainer");
   Projects.forEach((project) => {
-    output += `<div class='tile project${project["id"]}'>`;
+    output += `<div class='tile project${project["id"]} projectTile'>`;
     output += `<div class="projectTitle">`;
-    output += `<h1>${project["name"]}</h1>`;
-    output += `<h2>${project["description"]}</h2>`;
+    output += `<h1>${project["h1"]}</h1>`;
+    output += `<h2>${project["h2"]}</h2>`;
+    output += `</div>`;
+    output += `<div class="projectButtons">`;
+    output += `<a href='${project["projectUrl"]}' target="_blank">See in browser</a>`;
+    output += `<a href='${project["githubUrl"]}' target="_blank">See Repo</a>`;
+    output += `</div>`;
+    output += `<div>`;
+    output += `<p>${project["description"]}</p>`;
     output += `</div>`;
     output += `<div class="placment">`;
     output += `<figure class="projectsImageContainer">`;
@@ -131,6 +165,7 @@ function displayProjects() {
     output += `</div>`;
     output += `</div>`;
   });
+  //
 
   if (!projectsContainer) {
     console.error("projectsContainer element not found in the DOM");
@@ -143,7 +178,7 @@ function displayProgress() {
   let output = "";
   let progressContainer = document.querySelector(".progressContainer");
   Progress.forEach((progress) => {
-    output += `<div class='tile project${progress["id"]}'>`;
+    output += `<div class='tile progress${progress["id"]}'>`;
     output += `<div class="projectTitle">`;
     output += `<h1>${progress["name"]}</h1>`;
     output += `<h2>${progress["description"]}</h2>`;
