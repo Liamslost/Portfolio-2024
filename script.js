e2173b86b5f7c2a179e88fa85bc381496128c8d6
Liam O'Connell <liamslost@googlemail.com>
2024-12-07 15:21:39 +0000

    contact section working. mobile styling added for contact.

--- script.js
+++ script.js
@@ -231,3 +231,39 @@ function getTempBristol(){
     })
     .catch(error => console.log(error));
   };
+
+  window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+  }
+
+var form = document.getElementById("my-form");
+async function handleSubmit(event) {
+event.preventDefault();
+var status = document.getElementById("my-form-status");
+var data = new FormData(event.target);
+fetch(event.target.action, {
+  method: form.method,
+  body: data,
+  headers: {
+    'Accept': 'application/json'
+}
+}).then(response => {
+  if (response.ok) {
+    status.innerHTML = "I'll be in touch soon!";
+    form.reset()
+  } else {
+    response.json().then(data => {
+    if (Object.hasOwn(data, 'errors')) {
+      status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+    } else {
+      status.innerHTML = "Oops! There was a problem submitting your mail"
+    }
+  })
+}
+}).catch(error => {
+  status.innerHTML = "Oops! There was a problem submitting your mail"
+});
+}
+form.addEventListener("submit", handleSubmit)
